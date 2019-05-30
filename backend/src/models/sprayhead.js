8fb08beea330eeeec8ca3eea5b479e4cf2f3894a
nishadjamaldeen <anjamaldeen531@gmail.com>
2019-05-30 14:10:37 -0700

    Changing datatypes for React compat

--- backend/src/models/sprayhead.js
+++ backend/src/models/sprayhead.js
@@ -3,8 +3,8 @@
 const mongoose = require('mongoose');
 
 var SprayHeadSchema = new mongoose.Schema({
-    latitude: { type: Number },
-    longitude: { type: Number },
+    latitude: { type: String },
+    longitude: { type: String },
     state: { type: Boolean, default: false },
     created: { type: Date, default: Date.now },
     node_id: { type: String }
